9814d30e689f7297cb290c0ce3b7492d1c90b38a
VldSab <vladimir.saber@gmail.com>
2024-01-04 14:41:02 +0300

    Initial commit

---
+++ src/main/java/ru/saberullin/socialotusclub/SocialOtusClubApplication.java
@@ -0,0 +1,13 @@
+package ru.saberullin.socialotusclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOtusClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialOtusClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/saberullin/socialotusclub/SocialOtusClubApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saberullin.socialotusclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialOtusClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
